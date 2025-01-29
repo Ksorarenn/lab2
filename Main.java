d97bba3d4953725a91a72a9212205ed9780a17ce
Ksorarenn <153994645+Ksorarenn@users.noreply.github.com>
2025-01-29 12:01:35 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Write the value of x: ");
+        double x = in.nextDouble();
+        if(x > 1){
+            System.out.println("Write the value of n:");
+            double n = in.nextDouble();
+            double arcctg = Math.PI/2;
+            for(double i = 0;i < n; i++){
+                arcctg += Math.pow(-1,i+1) / ((2*i+1) * Math.pow(x,(2*i+1)));
+            }
+            System.out.println("Result: "+ arcctg);
+        }
+        else System.out.println("Value of x is incorrect");
+        }
+    }
